4decf090cc783fe5717d3b8171b981ddebd3d698
Barnaby Brachamul <barnaby.brachamul@gmail.com>
2016-05-27 17:45:26 +0200

    First Commit

---
+++ _project/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.generic.base import RedirectView
+
+admin.site.site_header = 'Fichier JDem - Administration'
+
+urlpatterns = [
+	url('^', include('django.contrib.auth.urls')),
+	url(r'^arriere-boutique/', admin.site.urls, name='admin'),
+	url(r'^fichiers-adherents/', include('fichiers_adherents.urls')),
+	url(r'^phoning/', include('phoning.urls')),
+	url(r'^$', RedirectView.as_view(url="/phoning", permanent=False)),
+]
